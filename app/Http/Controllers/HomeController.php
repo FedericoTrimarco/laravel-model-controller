aae783b5a7ed04df6af0cf96104bbace2fb5878e
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-01-21 15:27:11 +0100

    add model && controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class HomeController extends Controller
 {
     public function index(){
+
+        $movies = Movie::all();
+        dump($movies);
+
         return view('home');
     }
 }
